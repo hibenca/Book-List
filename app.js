5d9ed73fbe59e58ff9c9ec3eae35c3c02a36eaf9
Bluesmoke <43018850+hibenca@users.noreply.github.com>
2020-08-05 19:07:53 -0500

    Add constructors

--- app.js
+++ app.js
@@ -1,27 +1,76 @@
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const submit = document.getElementById("submit");
-const header = document.getElementById("header");
+// Book Constructor
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
 
+// UI Constructor
+class UI {
+    // Add book to list
+    addBookToList(book) {
+        const table = document.getElementById("table");
 
-submit.addEventListener("click", function(e) {
-    header.innerHTML = title.value;
-    e.preventDefault();
+        // Create tr element
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td><a href='#'>X</a></td>
+        `;
+
+        table.appendChild(row);
+
+    }
+
+    showAlert() {
+
+    }
+
+    deleteBook() {
+
+    }
+
+    clearFields() {
+        const title = document.getElementById("title").value = '';
+        const author = document.getElementById("author").value = '';
+    };
+}
+
+// Event Listeners
+document.getElementById("form-book").addEventListener("submit", function (e) {
+
+    // Get form values
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+
+    // Validate
+    if (title === '' || author === '') {
+
+        // Edit.......................
+        const newDiv = document.createElement('div');
+        const newDivContent = document.createTextNode("We need text bro");
+        newDiv.appendChild(newDivContent);
+
+        const currentDiv = document.getElementById("form-book");
+        document.body.insertBefore(newDiv, currentDiv);
+
+    } else {
+
+        // Instantiate book
+        const book = new Book(title, author);
+
+        // Intantiate UI
+        const ui = new UI();
+
+        // Add book to list
+        ui.addBookToList(book);
+
+        // Clear fields
+        ui.clearFields();
+
+        e.preventDefault();
+    }
 })
-
-// class Person {
-//     constructor(firstName, lastName) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//     }
-//     greeting() {
-//         return `Hello ${this.firstName} ${this.lastName}`
-//     }
-//     gotMarried(lastNameChange) {
-//         this.lastName = lastNameChange;
-//     }
-// }
-
-
-
-// const person1 = new Person("Corey", "Hiben")
